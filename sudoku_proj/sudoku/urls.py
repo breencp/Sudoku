570d5899730b69d3f9d1c1600a97ea3763f9823b
Christopher Breen <breencp@gmail.com>
2020-05-17 23:16:04 -0700

    Regular Update

---
+++ sudoku_proj/sudoku/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('howtoplay/', views.how_to_play, name='how_to_play'),
+    path('newgame/', views.new_game, name='new_game'),
+    path('leaderboard/', views.leaderboard, name='leaderboard'),
+    path('sudoku/', views.sudoku, name='sudoku')
+]
--- sudoku_proj/sudoku/views.py
+++ sudoku_proj/sudoku/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("Hello, World")
+
+
+def how_to_play(request):
+    return HttpResponse("How to Play")
+
+
+def new_game(request):
+    return HttpResponse("New Game")
+
+
+def leaderboard(request):
+    return HttpResponse("Leader Board")
+
+
+def sudoku(request):
+    return HttpResponse("Game Board")
