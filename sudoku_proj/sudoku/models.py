0bf4d1672dbd453818e622668827d5159dc393b1
Christopher Smith <razokoooko@gmail.com>
2020-05-20 08:35:36 -0400

    basic models added

--- sudoku_proj/sudoku/models.py
+++ sudoku_proj/sudoku/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class ID(models.Model):
+    id = models.IntegerField(primary_key=True)
+
+class User(models.Model):
+    id = models.ForeignKey(ID, primary_key=True, on_delete=models.CASCADE)
+    user = models.CharField(max_length=20)
+
+class Wins(models.Model):
+    id = models.ForeignKey(ID, primary_key=True, on_delete=models.CASCADE)
+    wins = models.IntegerField(default=0)
+
+class Losses(models.Model):
+    id = models.ForeignKey(ID, primary_key=True, on_delete=models.CASCADE)
+    losses = models.IntegerField(default=0)
