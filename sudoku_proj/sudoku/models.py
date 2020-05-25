e7ea4fc762b152d0f23a19253115590b18249598
Christopher Smith <razokoooko@gmail.com>
2020-05-24 23:50:49 -0400

    5/24/2020 Updated/corrected models.py

--- sudoku_proj/sudoku/models.py
+++ sudoku_proj/sudoku/models.py
@@ -1,16 +1,13 @@
 from django.db import models
 
-class ID(models.Model):
-    id = models.IntegerField(primary_key=True)
-
-class User(models.Model):
-    id = models.ForeignKey(ID, primary_key=True, on_delete=models.CASCADE)
+# User table with stats
+class UserInfo(models.Model):
+    uid = models.IntegerField(primary_key=True)
     user = models.CharField(max_length=20)
-
-class Wins(models.Model):
-    id = models.ForeignKey(ID, primary_key=True, on_delete=models.CASCADE)
     wins = models.IntegerField(default=0)
-
-class Losses(models.Model):
-    id = models.ForeignKey(ID, primary_key=True, on_delete=models.CASCADE)
     losses = models.IntegerField(default=0)
+
+# Saved game table
+class Board(models.Model):
+    bid = models.IntegerField(primary_key=True)
+    saved_board = models.CharField(max_length=1863)
