2c098b54899149349ce03908bd7330972dbb8f4a
Christopher Smith <razokoooko@gmail.com>
2020-06-09 09:15:50 -0400

    6/9/2020 created new table and modified existing table

--- sudoku_proj/sudoku/models.py
+++ sudoku_proj/sudoku/models.py
@@ -5,9 +5,16 @@
 from django.db import models
 
 
+# Puzzle table for storing pre-generated puzzles
+class Puzzles(models.Model):
+    puzz_id = models.CharField(primary_key=True)
+    puzzle = models.CharField(max_length=1863)
+    difficulty = models.CharField(max_length=1)
+
+
 # User table with stats for leaderboard
 class Played(models.Model):
-    puzz_id = models.CharField(foreign_key=True)
+    puzz_id = models.ForeignKey(Puzzles, on_delete=models.CASCADE)
     user = models.CharField(max_length=20)
     start_time = models.FloatField(default=0)
     end_time = models.FloatField(default=0)
@@ -15,10 +22,3 @@ class Played(models.Model):
     status = models.CharField(max_length=1)
     hints = models.IntegerField(default=0)
     difference = models.FloatField
-
-
-# Puzzle table for storing pre-generated puzzles
-class Puzzles(models.Model):
-    puzz_id = models.CharField(primary_key=True)
-    puzzle = models.CharField(max_length=1863)
-    difficulty = models.CharField(max_length=1)
