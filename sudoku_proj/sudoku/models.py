61faf1c052ab8a57afb59788df21a01f7fbf1967
Christopher Smith <razokoooko@gmail.com>
2020-06-09 12:16:27 -0400

    6/9/2020 individual techniques added as boolean fields

--- sudoku_proj/sudoku/models.py
+++ sudoku_proj/sudoku/models.py
@@ -7,16 +7,28 @@ from django.db import models
 
 # Puzzle table for storing pre-generated puzzles
 class Puzzles(models.Model):
-    id = models.AutoField(primary_key=True)
+    puzzle_id = models.AutoField(primary_key=True)
     board = models.CharField(max_length=1863)
     difficulty = models.CharField(max_length=1)
-    techniques = models.CharField
+    naked_single = models.BooleanField(default=False)
+    hidden_single = models.BooleanField(default=False)
+    naked_pair = models.BooleanField(default=False)
+    omission = models.BooleanField(default=False)
+    naked_triplet = models.BooleanField(default=False)
+    hidden_pair = models.BooleanField(default=False)
+    naked_quad = models.BooleanField(default=False)
+    hidden_triplet = models.BooleanField(default=False)
+    hidden_quad = models.BooleanField(default=False)
+    x_wing = models.BooleanField(default=False)
+    swordfish = models.BooleanField(default=False)
+    xy_wing = models.BooleanField(default=False)
+    unique_rectangle = models.BooleanField(default=False)
 
 
 # User table with stats for leaderboard determination
 class Played(models.Model):
-    p_id = models.AutoField(primary_key=True)
-    id = models.ForeignKey(Puzzles, on_delete=models.CASCADE)
+    played_id = models.AutoField(primary_key=True)
+    puzzle_id = models.ForeignKey(Puzzles, on_delete=models.CASCADE)
     user = models.CharField(max_length=20)
     start_time = models.IntegerField(default=0)
     end_time = models.IntegerField(default=0)
