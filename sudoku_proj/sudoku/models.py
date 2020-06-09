5d8fdaa23111b5330e0f33c4291c6b883ddfe6c5
Christopher Smith <razokoooko@gmail.com>
2020-06-09 11:26:04 -0400

    6/9/2020 p_id added as primary key for Played

--- sudoku_proj/sudoku/models.py
+++ sudoku_proj/sudoku/models.py
@@ -18,9 +18,8 @@ class Played(models.Model):
     p_id = models.AutoField(primary_key=True)
     id = models.ForeignKey(Puzzles, on_delete=models.CASCADE)
     user = models.CharField(max_length=20)
-    start_time = models.FloatField(default=0)
-    end_time = models.FloatField(default=0)
+    start_time = models.IntegerField(default=0)
+    end_time = models.IntegerField(default=0)
     saved_board = models.CharField(max_length=1863)
     status = models.CharField(max_length=1)
     hints = models.IntegerField(default=0)
-    difference = models.FloatField
