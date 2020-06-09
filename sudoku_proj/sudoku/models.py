7eac9306d992d6809979f4328321f40e2056a450
Christopher Smith <razokoooko@gmail.com>
2020-06-09 10:48:31 -0400

    6/9/2020 re-configured primary key, added techniques, and renamed board

--- sudoku_proj/sudoku/models.py
+++ sudoku_proj/sudoku/models.py
@@ -7,14 +7,15 @@ from django.db import models
 
 # Puzzle table for storing pre-generated puzzles
 class Puzzles(models.Model):
-    puzz_id = models.CharField(primary_key=True, max_length=5)
-    puzzle = models.CharField(max_length=1863)
+    id = models.AutoField(primary_key=True)
+    board = models.CharField(max_length=1863)
     difficulty = models.CharField(max_length=1)
+    techniques = models.CharField
 
 
 # User table with stats for leaderboard
 class Played(models.Model):
-    puzz_id = models.ForeignKey(Puzzles, on_delete=models.CASCADE)
+    id = models.ForeignKey(Puzzles, on_delete=models.CASCADE)
     user = models.CharField(max_length=20)
     start_time = models.FloatField(default=0)
     end_time = models.FloatField(default=0)
