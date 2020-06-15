09ac5d693b801969d296f48f27edb050be7b6fc6
Christopher Smith <razokoooko@gmail.com>
2020-06-15 14:53:11 -0400

    6/15/2020 creation of save_game function

--- sudoku_proj/sudoku/gamerecords.py
+++ sudoku_proj/sudoku/gamerecords.py
@@ -27,16 +27,21 @@ def get_game(difficulty):
     return puzzles.board, puzzles.solution
 
 
-def save_game():
+def save_game(data):
     saved_puzzle: Played = Played()
-    saved_puzzle.user = Played.user
-    saved_puzzle.start_time = Played.start_time
-    saved_puzzle.end_time = Played.end_time
-    saved_puzzle.saved_board = Played.saved_board
-    saved_puzzle.status = Played.status
-    saved_puzzle.hints = Played.hints
-    try:
+    for x in data:
+        saved_puzzle.user = x['user']
+        saved_puzzle.start_time = x['start_time']
+        saved_puzzle.end_time = x['end_time']
+        saved_puzzle.status = x['status']
+        saved_puzzle.hints = x['hints']
+        saved_puzzle.saved_board = x['current_board']
+        saved_puzzle.puzzle_id = x['orig_board']
+    query = Played.objects.filter(puzzle_id=saved_puzzle.puzzle_id, user=saved_puzzle.user,
+                                  start_time=saved_puzzle.start_time)
+    # update an existing entry
+    if query:
+        Played.objects.update(saved_puzzle)
+    # create a new entry
+    else:
         saved_puzzle.save()
-    except IntegrityError:
-        # user state already exists
-        saved_puzzle.save(force_update=True)
