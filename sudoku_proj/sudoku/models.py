bc1c8b2422e956c199a0fcce0c5b63663aef6888
Christopher Smith <razokoooko@gmail.com>
2020-06-15 23:09:16 -0400

    6/15/2020 refinement of models

--- sudoku_proj/sudoku/models.py
+++ sudoku_proj/sudoku/models.py
@@ -32,7 +32,7 @@ class Played(models.Model):
     puzzle_id = models.ForeignKey(Puzzles, on_delete=models.CASCADE)
     user = models.CharField(max_length=20)
     start_time = models.IntegerField(default=0)
-    end_time = models.IntegerField(default=0)
+    end_time = models.IntegerField
     saved_board = models.CharField(max_length=1863)
     status = models.CharField(max_length=1)
     hints = models.IntegerField(default=0)
