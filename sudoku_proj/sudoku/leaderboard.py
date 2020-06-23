dfd6f59eba2fc1870b16837e6e32ca87cb6be81e
Christopher Smith <razokoooko@gmail.com>
2020-06-23 13:49:53 -0400

    6/23/2020 modifications to leaderboard.py and views.py to allow better passing of data

--- sudoku_proj/sudoku/leaderboard.py
+++ sudoku_proj/sudoku/leaderboard.py
@@ -7,90 +7,116 @@ from django.db import connection
 
 # raw SQL queries to determine user, puzzle ID, and completion time of puzzles
 def calculate_leaders(difficulty):
+    users = ""
+    puzzle_ids = ""
+    completion_times = ""
     with connection.cursor() as cursor:
         if difficulty == 1:
-            cursor.execute("SELECT sudoku_played.user FROM sudoku_played JOIN sudoku_puzzles ON"
-                           " sudoku_played.puzzle_id_id = sudoku_puzzles.puzzle_id WHERE sudoku_puzzles.difficulty ="
-                           " 1 AND sudoku_played.end_time - sudoku_played.start_time IS NOT NULL ORDER BY"
-                           " sudoku_played.end_time - sudoku_played.start_time LIMIT 5")
-            users = cursor.fetchall()
-            cursor.execute("SELECT sudoku_played.puzzle_id_id FROM sudoku_played JOIN sudoku_puzzles ON"
-                           " sudoku_played.puzzle_id_id = sudoku_puzzles.puzzle_id WHERE sudoku_puzzles.difficulty ="
-                           " 1 AND sudoku_played.end_time - sudoku_played.start_time IS NOT NULL ORDER BY"
-                           " sudoku_played.end_time - sudoku_played.start_time LIMIT 5")
-            puzzle_ids = cursor.fetchall()
-            cursor.execute("SELECT sudoku_played.end_time - sudoku_played.start_time FROM sudoku_played JOIN"
-                           " sudoku_puzzles ON sudoku_played.puzzle_id_id = sudoku_puzzles.puzzle_id WHERE"
-                           " sudoku_puzzles.difficulty = 1 AND sudoku_played.end_time - sudoku_played.start_time"
-                           " IS NOT NULL ORDER BY sudoku_played.end_time - sudoku_played.start_time LIMIT 5")
-            completion_times = cursor.fetchall()
+            rows = cursor.execute("SELECT sudoku_played.user FROM sudoku_played JOIN sudoku_puzzles ON "
+                                  "sudoku_played.puzzle_id_id = sudoku_puzzles.puzzle_id WHERE "
+                                  "sudoku_puzzles.difficulty = 1 AND sudoku_played.end_time - sudoku_played.start_time "
+                                  "IS NOT NULL ORDER BY sudoku_played.end_time - sudoku_played.start_time LIMIT 5")
+            for row in rows:
+                users = tuple(users) + row
+            rows = cursor.execute("SELECT sudoku_played.puzzle_id_id FROM sudoku_played JOIN sudoku_puzzles ON "
+                                  "sudoku_played.puzzle_id_id = sudoku_puzzles.puzzle_id WHERE "
+                                  "sudoku_puzzles.difficulty = 1 AND sudoku_played.end_time - sudoku_played.start_time "
+                                  "IS NOT NULL ORDER BY sudoku_played.end_time - sudoku_played.start_time LIMIT 5")
+            for row in rows:
+                puzzle_ids = tuple(puzzle_ids) + row
+            rows = cursor.execute("SELECT sudoku_played.end_time - sudoku_played.start_time FROM sudoku_played JOIN "
+                                  "sudoku_puzzles ON sudoku_played.puzzle_id_id = sudoku_puzzles.puzzle_id WHERE "
+                                  "sudoku_puzzles.difficulty = 1 AND sudoku_played.end_time - sudoku_played.start_time "
+                                  "IS NOT NULL ORDER BY sudoku_played.end_time - sudoku_played.start_time LIMIT 5")
+            for row in rows:
+                completion_times = tuple(completion_times) + row
 
         elif difficulty == 2:
-            cursor.execute("SELECT sudoku_played.user FROM sudoku_played JOIN sudoku_puzzles ON"
-                           " sudoku_played.puzzle_id_id = sudoku_puzzles.puzzle_id WHERE sudoku_puzzles.difficulty ="
-                           " 2 AND sudoku_played.end_time - sudoku_played.start_time IS NOT NULL ORDER BY"
-                           " sudoku_played.end_time - sudoku_played.start_time LIMIT 5")
-            users = cursor.fetchall()
-            cursor.execute("SELECT sudoku_played.puzzle_id_id FROM sudoku_played JOIN sudoku_puzzles ON"
-                           " sudoku_played.puzzle_id_id = sudoku_puzzles.puzzle_id WHERE sudoku_puzzles.difficulty ="
-                           " 2 AND sudoku_played.end_time - sudoku_played.start_time IS NOT NULL ORDER BY"
-                           " sudoku_played.end_time - sudoku_played.start_time LIMIT 5")
-            puzzle_ids = cursor.fetchall()
-            cursor.execute("SELECT sudoku_played.end_time - sudoku_played.start_time FROM sudoku_played JOIN"
-                           " sudoku_puzzles ON sudoku_played.puzzle_id_id = sudoku_puzzles.puzzle_id WHERE"
-                           " sudoku_puzzles.difficulty = 2 AND sudoku_played.end_time - sudoku_played.start_time"
-                           " IS NOT NULL ORDER BY sudoku_played.end_time - sudoku_played.start_time LIMIT 5")
-            completion_times = cursor.fetchall()
+            rows = cursor.execute("SELECT sudoku_played.user FROM sudoku_played JOIN sudoku_puzzles ON "
+                                  "sudoku_played.puzzle_id_id = sudoku_puzzles.puzzle_id WHERE "
+                                  "sudoku_puzzles.difficulty = 2 AND sudoku_played.end_time - sudoku_played.start_time "
+                                  "IS NOT NULL ORDER BY sudoku_played.end_time - sudoku_played.start_time LIMIT 5")
+            for row in rows:
+                users = tuple(users) + row
+
+            rows = cursor.execute("SELECT sudoku_played.puzzle_id_id FROM sudoku_played JOIN sudoku_puzzles ON "
+                                  "sudoku_played.puzzle_id_id = sudoku_puzzles.puzzle_id WHERE "
+                                  "sudoku_puzzles.difficulty = 2 AND sudoku_played.end_time - sudoku_played.start_time "
+                                  "IS NOT NULL ORDER BY sudoku_played.end_time - sudoku_played.start_time LIMIT 5")
+            for row in rows:
+                puzzle_ids = tuple(puzzle_ids) + row
+
+            rows = cursor.execute("SELECT sudoku_played.end_time - sudoku_played.start_time FROM sudoku_played JOIN "
+                                  "sudoku_puzzles ON sudoku_played.puzzle_id_id = sudoku_puzzles.puzzle_id WHERE "
+                                  "sudoku_puzzles.difficulty = 2 AND sudoku_played.end_time - sudoku_played.start_time "
+                                  "IS NOT NULL ORDER BY sudoku_played.end_time - sudoku_played.start_time LIMIT 5")
+            for row in rows:
+                completion_times = tuple(completion_times) + row
 
         elif difficulty == 3:
-            cursor.execute("SELECT sudoku_played.user FROM sudoku_played JOIN sudoku_puzzles ON"
-                           " sudoku_played.puzzle_id_id = sudoku_puzzles.puzzle_id WHERE sudoku_puzzles.difficulty ="
-                           " 3 AND sudoku_played.end_time - sudoku_played.start_time IS NOT NULL ORDER BY"
-                           " sudoku_played.end_time - sudoku_played.start_time LIMIT 5")
-            users = cursor.fetchall()
-            cursor.execute("SELECT sudoku_played.puzzle_id_id FROM sudoku_played JOIN sudoku_puzzles ON"
-                           " sudoku_played.puzzle_id_id = sudoku_puzzles.puzzle_id WHERE sudoku_puzzles.difficulty ="
-                           " 3 AND sudoku_played.end_time - sudoku_played.start_time IS NOT NULL ORDER BY"
-                           " sudoku_played.end_time - sudoku_played.start_time LIMIT 5")
-            puzzle_ids = cursor.fetchall()
-            cursor.execute("SELECT sudoku_played.end_time - sudoku_played.start_time FROM sudoku_played JOIN"
-                           " sudoku_puzzles ON sudoku_played.puzzle_id_id = sudoku_puzzles.puzzle_id WHERE"
-                           " sudoku_puzzles.difficulty = 3 AND sudoku_played.end_time - sudoku_played.start_time"
-                           " IS NOT NULL ORDER BY sudoku_played.end_time - sudoku_played.start_time LIMIT 5")
-            completion_times = cursor.fetchall()
+            rows = cursor.execute("SELECT sudoku_played.user FROM sudoku_played JOIN sudoku_puzzles ON "
+                                  "sudoku_played.puzzle_id_id = sudoku_puzzles.puzzle_id WHERE "
+                                  "sudoku_puzzles.difficulty = 3 AND sudoku_played.end_time - sudoku_played.start_time "
+                                  "IS NOT NULL ORDER BY sudoku_played.end_time - sudoku_played.start_time LIMIT 5")
+            for row in rows:
+                users = tuple(users) + row
+
+            rows = cursor.execute("SELECT sudoku_played.puzzle_id_id FROM sudoku_played JOIN sudoku_puzzles ON "
+                                  "sudoku_played.puzzle_id_id = sudoku_puzzles.puzzle_id WHERE "
+                                  "sudoku_puzzles.difficulty = 3 AND sudoku_played.end_time - sudoku_played.start_time "
+                                  "IS NOT NULL ORDER BY sudoku_played.end_time - sudoku_played.start_time LIMIT 5")
+            for row in rows:
+                puzzle_ids = tuple(puzzle_ids) + row
+
+            rows = cursor.execute("SELECT sudoku_played.end_time - sudoku_played.start_time FROM sudoku_played JOIN "
+                                  "sudoku_puzzles ON sudoku_played.puzzle_id_id = sudoku_puzzles.puzzle_id WHERE "
+                                  "sudoku_puzzles.difficulty = 3 AND sudoku_played.end_time - sudoku_played.start_time "
+                                  "IS NOT NULL ORDER BY sudoku_played.end_time - sudoku_played.start_time LIMIT 5")
+            for row in rows:
+                completion_times = tuple(completion_times) + row
 
         elif difficulty == 4:
-            cursor.execute("SELECT sudoku_played.user FROM sudoku_played JOIN sudoku_puzzles ON"
-                           " sudoku_played.puzzle_id_id = sudoku_puzzles.puzzle_id WHERE sudoku_puzzles.difficulty ="
-                           " 4 AND sudoku_played.end_time - sudoku_played.start_time IS NOT NULL ORDER BY"
-                           " sudoku_played.end_time - sudoku_played.start_time LIMIT 5")
-            users = cursor.fetchall()
-            cursor.execute("SELECT sudoku_played.puzzle_id_id FROM sudoku_played JOIN sudoku_puzzles ON"
-                           " sudoku_played.puzzle_id_id = sudoku_puzzles.puzzle_id WHERE sudoku_puzzles.difficulty ="
-                           " 4 AND sudoku_played.end_time - sudoku_played.start_time IS NOT NULL ORDER BY"
-                           " sudoku_played.end_time - sudoku_played.start_time LIMIT 5")
-            puzzle_ids = cursor.fetchall()
-            cursor.execute("SELECT sudoku_played.end_time - sudoku_played.start_time FROM sudoku_played JOIN"
-                           " sudoku_puzzles ON sudoku_played.puzzle_id_id = sudoku_puzzles.puzzle_id WHERE"
-                           " sudoku_puzzles.difficulty = 4 AND sudoku_played.end_time - sudoku_played.start_time"
-                           " IS NOT NULL ORDER BY sudoku_played.end_time - sudoku_played.start_time LIMIT 5")
-            completion_times = cursor.fetchall()
+            rows = cursor.execute("SELECT sudoku_played.user FROM sudoku_played JOIN sudoku_puzzles ON "
+                                  "sudoku_played.puzzle_id_id = sudoku_puzzles.puzzle_id WHERE "
+                                  "sudoku_puzzles.difficulty = 4 AND sudoku_played.end_time - sudoku_played.start_time "
+                                  "IS NOT NULL ORDER BY sudoku_played.end_time - sudoku_played.start_time LIMIT 5")
+            for row in rows:
+                users = tuple(users) + row
+
+            rows = cursor.execute("SELECT sudoku_played.puzzle_id_id FROM sudoku_played JOIN sudoku_puzzles ON "
+                                  "sudoku_played.puzzle_id_id = sudoku_puzzles.puzzle_id WHERE "
+                                  "sudoku_puzzles.difficulty = 4 AND sudoku_played.end_time - sudoku_played.start_time "
+                                  "IS NOT NULL ORDER BY sudoku_played.end_time - sudoku_played.start_time LIMIT 5")
+            for row in rows:
+                puzzle_ids = tuple(puzzle_ids) + row
+
+            rows = cursor.execute("SELECT sudoku_played.end_time - sudoku_played.start_time FROM sudoku_played JOIN "
+                                  "sudoku_puzzles ON sudoku_played.puzzle_id_id = sudoku_puzzles.puzzle_id WHERE "
+                                  "sudoku_puzzles.difficulty = 4 AND sudoku_played.end_time - sudoku_played.start_time "
+                                  "IS NOT NULL ORDER BY sudoku_played.end_time - sudoku_played.start_time LIMIT 5")
+            for row in rows:
+                completion_times = tuple(completion_times) + row
 
         elif difficulty == 5:
-            cursor.execute("SELECT sudoku_played.user FROM sudoku_played JOIN sudoku_puzzles ON"
-                           " sudoku_played.puzzle_id_id = sudoku_puzzles.puzzle_id WHERE sudoku_puzzles.difficulty ="
-                           " 5 AND sudoku_played.end_time - sudoku_played.start_time IS NOT NULL ORDER BY"
-                           " sudoku_played.end_time - sudoku_played.start_time LIMIT 5")
-            users = cursor.fetchall()
-            cursor.execute("SELECT sudoku_played.puzzle_id_id FROM sudoku_played JOIN sudoku_puzzles ON"
-                           " sudoku_played.puzzle_id_id = sudoku_puzzles.puzzle_id WHERE sudoku_puzzles.difficulty ="
-                           " 5 AND sudoku_played.end_time - sudoku_played.start_time IS NOT NULL ORDER BY"
-                           " sudoku_played.end_time - sudoku_played.start_time LIMIT 5")
-            puzzle_ids = cursor.fetchall()
-            cursor.execute("SELECT sudoku_played.end_time - sudoku_played.start_time FROM sudoku_played JOIN"
-                           " sudoku_puzzles ON sudoku_played.puzzle_id_id = sudoku_puzzles.puzzle_id WHERE"
-                           " sudoku_puzzles.difficulty = 5 AND sudoku_played.end_time - sudoku_played.start_time"
-                           " IS NOT NULL ORDER BY sudoku_played.end_time - sudoku_played.start_time LIMIT 5")
-            completion_times = cursor.fetchall()
+            rows = cursor.execute("SELECT sudoku_played.user FROM sudoku_played JOIN sudoku_puzzles ON "
+                                  "sudoku_played.puzzle_id_id = sudoku_puzzles.puzzle_id WHERE "
+                                  "sudoku_puzzles.difficulty = 5 AND sudoku_played.end_time - sudoku_played.start_time "
+                                  "IS NOT NULL ORDER BY sudoku_played.end_time - sudoku_played.start_time LIMIT 5")
+            for row in rows:
+                users = tuple(users) + row
+
+            rows = cursor.execute("SELECT sudoku_played.puzzle_id_id FROM sudoku_played JOIN sudoku_puzzles ON "
+                                  "sudoku_played.puzzle_id_id = sudoku_puzzles.puzzle_id WHERE "
+                                  "sudoku_puzzles.difficulty = 5 AND sudoku_played.end_time - sudoku_played.start_time "
+                                  "IS NOT NULL ORDER BY sudoku_played.end_time - sudoku_played.start_time LIMIT 5")
+            for row in rows:
+                puzzle_ids = tuple(puzzle_ids) + row
+
+            rows = cursor.execute("SELECT sudoku_played.end_time - sudoku_played.start_time FROM sudoku_played JOIN "
+                                  "sudoku_puzzles ON sudoku_played.puzzle_id_id = sudoku_puzzles.puzzle_id WHERE "
+                                  "sudoku_puzzles.difficulty = 5 AND sudoku_played.end_time - sudoku_played.start_time "
+                                  "IS NOT NULL ORDER BY sudoku_played.end_time - sudoku_played.start_time LIMIT 5")
+            for row in rows:
+                completion_times = tuple(completion_times) + row
 
     return users, puzzle_ids, completion_times
