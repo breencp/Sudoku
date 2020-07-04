14cb292a547261981b7ee31987ddcd2261301816
Christopher Smith <razokoooko@gmail.com>
2020-07-04 08:00:01 -0400

    7/4/2020 Query modified to return only those games where no hints were used and function greatly simplified to optimize efficiency

--- sudoku_proj/sudoku/leaderboard.py
+++ sudoku_proj/sudoku/leaderboard.py
@@ -7,80 +7,20 @@ import datetime
 from django.db import connection
 
 
+# modifications by Christopher Smith for Sprint 2
 # raw SQL queries to determine user, puzzle ID, and completion time of puzzles
 def calculate_leaders(difficulty):
     with connection.cursor() as cursor:
-        if difficulty == 1:
-            record = [dict()]
-            query = cursor.execute(
-                "SELECT sudoku_played.user, sudoku_played.puzzle_id_id, sudoku_played.end_time - "
-                "sudoku_played.start_time FROM sudoku_played JOIN sudoku_puzzles ON sudoku_played.puzzle_id_id = "
-                "sudoku_puzzles.puzzle_id WHERE sudoku_puzzles.difficulty = 1 AND sudoku_played.end_time - "
-                "sudoku_played.start_time IS NOT NULL ORDER BY sudoku_played.end_time - sudoku_played.start_time "
-                "LIMIT 5")
-            rownr = 0
-            for row in query:
-                record.insert(rownr, {'user': row[0], 'puzzle_id': row[1], 'completion_time':
-                    datetime.timedelta(seconds=row[2])})
-                rownr += 1
-            return record
-
-        if difficulty == 2:
-            record = [dict()]
-            query = cursor.execute(
-                "SELECT sudoku_played.user, sudoku_played.puzzle_id_id, sudoku_played.end_time - "
-                "sudoku_played.start_time FROM sudoku_played JOIN sudoku_puzzles ON sudoku_played.puzzle_id_id = "
-                "sudoku_puzzles.puzzle_id WHERE sudoku_puzzles.difficulty = 2 AND sudoku_played.end_time - "
-                "sudoku_played.start_time IS NOT NULL ORDER BY sudoku_played.end_time - sudoku_played.start_time "
-                "LIMIT 5")
-            rownr = 0
-            for row in query:
-                record.insert(rownr, {'user': row[0], 'puzzle_id': row[1], 'completion_time':
-                    datetime.timedelta(seconds=row[2])})
-                rownr += 1
-            return record
-
-        if difficulty == 3:
-            record = [dict()]
-            query = cursor.execute(
-                "SELECT sudoku_played.user, sudoku_played.puzzle_id_id, sudoku_played.end_time - "
-                "sudoku_played.start_time FROM sudoku_played JOIN sudoku_puzzles ON sudoku_played.puzzle_id_id = "
-                "sudoku_puzzles.puzzle_id WHERE sudoku_puzzles.difficulty = 3 AND sudoku_played.end_time - "
-                "sudoku_played.start_time IS NOT NULL ORDER BY sudoku_played.end_time - sudoku_played.start_time "
-                "LIMIT 5")
-            rownr = 0
-            for row in query:
-                record.insert(rownr, {'user': row[0], 'puzzle_id': row[1], 'completion_time':
-                    datetime.timedelta(seconds=row[2])})
-                rownr += 1
-            return record
-
-        if difficulty == 4:
-            record = [dict()]
-            query = cursor.execute(
-                "SELECT sudoku_played.user, sudoku_played.puzzle_id_id, sudoku_played.end_time - "
-                "sudoku_played.start_time FROM sudoku_played JOIN sudoku_puzzles ON sudoku_played.puzzle_id_id = "
-                "sudoku_puzzles.puzzle_id WHERE sudoku_puzzles.difficulty = 4 AND sudoku_played.end_time - "
-                "sudoku_played.start_time IS NOT NULL ORDER BY sudoku_played.end_time - sudoku_played.start_time "
-                "LIMIT 5")
-            rownr = 0
-            for row in query:
-                record.insert(rownr, {'user': row[0], 'puzzle_id': row[1], 'completion_time':
-                    datetime.timedelta(seconds=row[2])})
-                rownr += 1
-            return record
-
-        if difficulty == 5:
-            record = [dict()]
-            query = cursor.execute(
-                "SELECT sudoku_played.user, sudoku_played.puzzle_id_id, sudoku_played.end_time - "
-                "sudoku_played.start_time FROM sudoku_played JOIN sudoku_puzzles ON sudoku_played.puzzle_id_id = "
-                "sudoku_puzzles.puzzle_id WHERE sudoku_puzzles.difficulty = 5 AND sudoku_played.end_time - "
-                "sudoku_played.start_time IS NOT NULL ORDER BY sudoku_played.end_time - sudoku_played.start_time "
-                "LIMIT 5")
-            rownr = 0
-            for row in query:
-                record.insert(rownr, {'user': row[0], 'puzzle_id': row[1], 'completion_time':
-                    datetime.timedelta(seconds=row[2])})
-                rownr += 1
-            return record
+        record = [dict()]
+        query = cursor.execute(
+            "SELECT sudoku_played.user, sudoku_played.puzzle_id_id, sudoku_played.end_time - "
+            "sudoku_played.start_time FROM sudoku_played JOIN sudoku_puzzles ON sudoku_played.puzzle_id_id = "
+            "sudoku_puzzles.puzzle_id WHERE sudoku_played.hints = 0 AND sudoku_puzzles.difficulty = %s AND "
+            "sudoku_played.end_time - sudoku_played.start_time IS NOT NULL ORDER BY sudoku_played.end_time - "
+            "sudoku_played.start_time LIMIT 5", [difficulty])
+        rownr = 0
+        for row in query:
+            record.insert(rownr, {'user': row[0], 'puzzle_id': row[1], 'completion_time':
+                datetime.timedelta(seconds=row[2])})
+            rownr += 1
+        return record
